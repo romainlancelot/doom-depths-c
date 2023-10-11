5ccc4c22ea13dcf3c6e68d4b520e6e3a3fa1df3f
Romain Lancelot <rlancelot@myges.fr>
2023-10-11 21:47:27 +0200

    add colors

--- src/include/display_standars.h
+++ src/include/display_standars.h
@@ -5,4 +5,8 @@
 #define CLEAR_SCREEN printf("\e[1;1H\e[2J");
 #define SEPARATEUR printf(" -------------------------------------------- \n\n");
 
+#define RED "\033[1;31m"
+#define BLUE "\033[1;34m"
+#define NORMAL "\033[0m"
+
 #endif
